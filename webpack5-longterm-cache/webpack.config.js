d6fa5e1996ed73095fe28a6e2d49a76cd8a18217
刘灿彬 <liucanbin@cvte.com>
2021-04-22 20:27:02 +0800

    [modify]更新配置

--- webpack5-longterm-cache/webpack.config.js
+++ webpack5-longterm-cache/webpack.config.js
@@ -1,14 +1,28 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: path.resolve(__dirname, "./index.js"),
 	mode: "development",
 	output: {
 		path: path.resolve(__dirname, "./dist"),
-		filename: "main.js",
+		filename: "[name].[contenthash].js",
 	},
 	optimization: {
 		moduleIds: "deterministic",
 		chunkIds: "deterministic",
 	},
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 9000,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "index.html"),
+			filename: "index.html",
+		}),
+		new CleanWebpackPlugin(),
+	],
 };
